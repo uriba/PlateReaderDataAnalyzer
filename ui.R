1d3df3894f6b6951ed8215efdb23f6fecd54318d
uri barenholz <uri.barenholz@gmail.com>
2015-07-21 10:41:13 +0300

    updated tooltip help

--- ui.R
+++ ui.R
@@ -18,8 +18,8 @@ shinyUI(fluidPage(
       conditionalPanel(
         condition="output.fileUploaded",
         bsTooltip(id="descfile",title="Upload optional plate description file (Excel) to label wells",placement="right", trigger="hover"),
-        bsTooltip(id="wellsToAnalyse",title="Which wells to display data on. Restricting the analysis to fewer wells increases the site's responsiveness",placement="right", trigger="hover"),
         fileInput("descfile","Plate description file"),
+        bsTooltip(id="wellsToAnalyse",title="Which wells to display data on. Restricting the analysis to fewer wells increases the site's responsiveness",placement="right", trigger="hover"),
         selectInput(
           inputId = "wellsToAnalyse",
           label = "Wells to analyse",
@@ -32,8 +32,7 @@ shinyUI(fluidPage(
         ),
         conditionalPanel(
           condition='input.wellsToAnalyse == "Match"',
-          bsTooltip(id="match",title="Regular expression to match",placement="right", trigger="hover"),
-          textInput("match","Expression to match for wells to analyse",value="A1")
+          textInput("match","Expression to search for wells to analyse",value="A1")
         ),
         conditionalPanel(
           condition='input.wellsToAnalyse == "Row"',
@@ -52,9 +51,9 @@ shinyUI(fluidPage(
           ),
         conditionalPanel(
           condition='input.analysisType == "Growth rate analysis" || input.analysisType == "Plotly test"',
-        bsTooltip(id="labelSelect",title="Which measurement label should be used for the analysis",placement="right", trigger="hover"),
           htmlOutput("labelSelect"),
-        bsTooltip(id="backgroundMethod",title="How the background level of each well should be determined",placement="right", trigger="hover"),
+          bsTooltip(id="labelSelect",title="Which measurement label should be used for the analysis",placement="right", trigger="hover"),
+          bsTooltip(id="backgroundMethod",title="How the background level, needed for analysis of growth rate of each well should be determined. Options are: for each well, use the average of the first few measurements - good when uneven pipetting or different media is used in different wells, time average of blank wells allows the user to specify a list of wells the average of the values of which are used, point-wise average of blank wells will calculate the average of the blank wells at each time-point and subtract this value from the analyzed wells - good for measurements with fluctuations in time, manual value uses a user-supplied value as the background for analysis.",placement="bottom", trigger="hover"),
           selectInput(
             inputId = "backgroundMethod",
             label = "Background subtraction method",
@@ -66,14 +65,17 @@ shinyUI(fluidPage(
             ),
           conditionalPanel(
             condition='input.backgroundMethod == "Manual value"',
+            bsTooltip(id="manualBackground",title="Use the supplied value as the background for all data points",placement="right", trigger="hover"),
             textInput("manualBackground","Background value to use",value="0.0")            
             ),
           conditionalPanel(
             condition='input.backgroundMethod == "Time average of blank wells" || input.backgroundMethod == "Point-wise average of blank wells"',
+            bsTooltip(id="blankWells",title="Comma-seperated, case-sensitive list of wells to use for determining background values",placement="right", trigger="hover"),
             textInput("blankWells","Blank wells to use",value="A1")            
             ),
           conditionalPanel(
             condition='input.backgroundMethod == "Average of first measurements of well"',
+            bsTooltip(id="perWellMesNum",title="Number of measurement points, starting at the first measurement, to take the average of and use as background value per well",placement="right", trigger="hover"),
             textInput("perWellMesNum","Number of measurements to use",value="1")            
             ),
           checkboxGroupInput("grPlots","Plots to display",c("raw",
@@ -86,9 +88,11 @@ shinyUI(fluidPage(
                              selected=c("background subtracted log",
                                         "doubling time vs. time",
                                         "doubling time vs. value")),
+          bsTooltip(id="windowSize",title="Number of measurement points to use in the running window determining the growth rate and doubling time",placement="right", trigger="hover"),
           textInput("windowSize","Growth rate window size",value="5"),
           checkboxInput("errorBars","Show error bars"),
           textInput("maxdtime","Maximum doubling time in minutes",value="300"),
+          bsTooltip(id="rsquare",title="R-square threshold below which data points will not be displayed",placement="right", trigger="hover"),
           textInput("rsquare","R-square threshold",value="0.9")
           )
         )
